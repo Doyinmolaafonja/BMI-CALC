b0bc4fddae6ee99df479b4f9ee7b4053ba7cd399
Afonja Doyinmola <doyinmolaafonj@gmail.com>
2020-07-21 23:36:37 +0100

    BMI calculator

---
+++ BMI calculator/src/MyBmiApplication.java
@@ -0,0 +1,43 @@
+import java.util.Scanner; 
+import java.util.Scanner; 
+
+public class MyBmiApplication {
+public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	Scanner input2 = new Scanner(System.in);
+	
+	System.out.println("KG: ");
+	System.out.println("M:");
+	
+	double number = input.nextDouble();
+	double number2 = input2.nextDouble();
+	double square = number2*number2;
+	double division = number/square;
+	//metre square 
+	
+	  
+System.out.println("KG:"+ number);
+System.out.println("M:"+ number2*number2);
+System.out.println("BMI = " + division);
+
+
+
+
+
+	
+ 
+
+
+	
+		
+		
+		 
+		
+
+	
+	
+	
+
+	
+}
+}
